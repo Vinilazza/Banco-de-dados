be7a62bda148976abefd9fbd83af8ebc642f798f
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2024-03-05 19:35:20 -0300

    Atividade 1

---
+++ ViniciusLazarotto.sql
@@ -0,0 +1,2 @@
+create database SIS2402;
+create table clientes ( nome varchar(50),email varchar(30),dataNasc integer(8),sexo varchar(30),cep varchar(30),rua varchar(30),numeroCasa integer,bairro varchar(30),cidade varchar(30),uf varchar(2),pais varchar(30));
